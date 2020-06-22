39c828d21a34d0e297059090b4b76f19de1ea6d4
Kimo Silva <e.k.silva441@gmail.com>
2020-06-22 12:49:53 -0700

    basic component structure and html written

---
+++ client/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Review from './components/Review.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
